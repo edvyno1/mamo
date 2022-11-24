503b978d551919c82810ee26f1dc89003cf27f86
1Lukukas <88420914+1Lukukas@users.noreply.github.com>
2022-11-24 22:31:27 +0200

    add login page, components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Layout from './components/Layout';
 import "./index.css";
-import Root from './routes/root';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>
 );
