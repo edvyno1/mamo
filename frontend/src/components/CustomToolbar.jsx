5ea115d6cae90d934075545fbfcfc58e8517a9c2
1Lukukas <88420914+1Lukukas@users.noreply.github.com>
2022-11-27 12:43:28 +0200

    add user creation

--- frontend/src/components/CustomToolbar.jsx
+++ frontend/src/components/CustomToolbar.jsx
@@ -2,23 +2,16 @@ import React from "react";
 import { IconButton, Tooltip } from '@mui/material';
 import { Add } from '@mui/icons-material';
 
-class CustomToolbar extends React.Component {
-  
-  handleClick = () => {
-    console.log("clicked on icon!");
-  }
-
-  render() {
+const CustomToolbar = ({handleClick}) => {
     return (
       <React.Fragment>
-        <Tooltip title={"custom icon"}>
-          <IconButton onClick={this.handleClick}>
+        <Tooltip title={"Pridėti"}>
+          <IconButton onClick={handleClick}>
             <Add />
           </IconButton>
         </Tooltip>
       </React.Fragment>
     );
-  }
 }
 
 export default CustomToolbar;
